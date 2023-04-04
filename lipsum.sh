300c0b76bd6c5a72fc232724617df6c304881ab9
marianodanielcejas <marian_cejas_93@hotmail.com.ar>
2023-04-04 14:42:59 -0300

    generando varios archivos

--- lipsum.sh
+++ lipsum.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-lorem=$(curl -s https://www.lipsum.com/feed/xml?ammount=2 | sed -n '/<lipsum>/,/<\/lipsum>/p' | sed -e 's/<lipsum>//g' -e 's/<\/lipsum>//g')
-
-echo "$lorem" > lorem.txt
+for i in {1..5}
+do
+  lorem=$(curl -s https://www.lipsum.com/feed/xml?ammount=2 | sed -n '/<lipsum>/,/<\/lipsum>/p' | sed -e 's/<lipsum>//g' -e 's/<\/lipsum>//g'
+  )
+  echo "$lorem" >> lorem.txt-$i.txt
+done
+  
 
